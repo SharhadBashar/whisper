807abc4804e56311f9ce557a15debb49fad6d7f0
sharhad <sharhad.bashar@uwaterloo.ca>
2023-02-09 01:52:14 -0500

    Files organized, steps outlined

---
+++ python/main.py
@@ -0,0 +1,15 @@
+'''
+to do:
+1. text to speech (tts)
+	a. get audio file input
+	b. run it through whisper
+	c. save dict in pkl format
+2. get categories (gc)
+	a. load dict
+	b. clean text
+		regex, clean, stopwords, lemmatize
+	c. get top 5 repeted words
+	d. get closest score for each in sql table
+	e. return highest cat score
+'''
+
